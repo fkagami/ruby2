9b40fba4dfad53efeed6c6f62c3a828012738864
fkagami <kagami1990@gmail.com>
2015-04-29 14:46:34 +0900

    P248

--- bsearch_0.rb
+++ bsearch_0.rb
@@ -1,19 +1,28 @@
-def bsearch(data_to_find, data, startIdx, endIdx)
-  return -1 if startIdx > endIdx
-  centerIdx = (startIdx + endIdx) / 2
-  return centerIdx if data[centerIdx] == data_to_find
-  if data[centerIdx] > data_to_find
-    bsearch(data_to_find, data, startIdx, centerIdx - 1)
+require "pry"
+
+def bsearch(data, i)
+  # sorted data is array
+  # at first find center id
+  center = data.size / 2
+  if data[center] == i
+    puts "true#{center + 1}番目にあった"
+  elsif data[center] > i
+    (0..center).each do |idx|
+      if data[idx] == i
+        puts "true#{idx + 1}番目にあった"
+      end
+    end
   else
-    bsearch(data_to_find, data, centerIdx + 1, endIdx)
+    (center..(data.size - 1)).each do |idx|
+      if data[idx] == i
+        puts "true #{idx + 1}番目にあった"
+      end
+    end
   end
 end
 
-data = [1, 3, 5, 7, 9]
-data_to_find = 7
-
-puts "#{data_to_find}は、#{data}の#{bsearch(data_to_find, data, 0, data.size - 1)}番目の要素です}"
-
-data_to_find = 6
-
-puts "#{data_to_find}は、#{data}の#{bsearch(data_to_find, data, 0, data.size - 1)}番目の要素です}"
+data = [1,2,3,4,5,6,7,8,9,10]
+bsearch(data, 1)
+bsearch(data, 4)
+bsearch(data, 5)
+bsearch(data, 6)
